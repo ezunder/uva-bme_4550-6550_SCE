3cc552e744a9ea43d7b46599063be7046bf9cf6e
Eli Zunder <ezunder@virginia.edu>
2023-10-04 15:06:53 -0400

    kasjfhlask

--- ETiX.R
+++ ETiX.R
@@ -4,12 +4,22 @@
 # Test add a new comment
 
 library(Seurat)
+library(data.table)
+
 
 # Location of data files
 setwd("G:\\My Drive/Working/ETX/")
 
+# Read in dataset
+data_in <- suppressWarnings(fread("./GSM5701522_ETiX_d5_1.inex.txt.gz", data.table=FALSE, showProgress=FALSE))
+
+# Clean up by converting gene names (first column) to row names
+first_col_name <- colnames(data_in[1])
+data_in <- data_in %>% column_to_rownames(first_col_name)
+
+colnames(data_in) <- gsub("_(?=[^.]*_)", "-", colnames(data_in), perl=TRUE)
 
 
 
-
+remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
 
